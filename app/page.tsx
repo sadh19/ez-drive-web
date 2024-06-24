74430b9d55df7cffb91d1f7184df9efe3c5638c6
sadh19 <saulalberto19@gmail.com>
2024-06-24 17:02:00 -0600

    - ADD Boostrap Grid system

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-custom-blue-bg">
-      <span className="text-custom-blue-3">Soy texto</span>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-3">
+            <span className="text-custom-blue-3">Soy texto</span>
+          </div>
+          <div className="col-sm-6">
+            <span className="text-custom-blue-3">Soy texto</span>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
